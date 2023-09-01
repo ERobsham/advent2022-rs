dfdca504e3a9f633871bbe9ed82a56c292365653
Earl Robsham <RobshamEnterprises@gmail.com>
2023-09-01 18:13:06 -0400

    add some sudo benchmarks
    just to get an idea of how long these `solve()` functions run for.

--- src/lib.rs
+++ src/lib.rs
@@ -57,3 +57,278 @@ impl Solve for Day {
         }
     }    
 }
+
+#[cfg(test_output_bench)]
+#[cfg(test)]
+mod bench {
+    use std::{
+        fs,
+        time::Instant, 
+    };
+    use crate::Part;
+    
+    fn test_iter(n: u8) -> Option<Box<dyn Iterator<Item = String>>> {
+        let path = format!("input/day-{:02}", n);
+        
+        let input_file = fs::read_to_string(path.as_str()).ok()?;
+        let lines:Vec<String> = input_file.split('\n')
+        .map(|item| String::from(item)).collect();
+    let input = Box::new(lines.into_iter());
+    
+    Some(input)
+    }
+
+    #[test]
+    fn test_day_01_pt1() {
+        use crate::day01::solve;
+        let iter = test_iter(1).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part1);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day01 Part1 duration: {:#?} \n", time_taken);
+    }
+    #[test]
+    fn test_day_01_pt2() {
+        use crate::day01::solve;
+        let iter = test_iter(1).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part2);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day01 Part2 duration: {:#?} \n", time_taken);
+    }
+
+    #[test]
+    fn test_day_02_pt1() {
+        use crate::day02::solve;
+        let iter = test_iter(2).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part1);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day02 Part1 duration: {:#?} \n", time_taken);
+    }
+    #[test]
+    fn test_day_02_pt2() {
+        use crate::day02::solve;
+        let iter = test_iter(2).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part2);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day02 Part2 duration: {:#?} \n", time_taken);
+    }
+
+    #[test]
+    fn test_day_03_pt1() {
+        use crate::day03::solve;
+        let iter = test_iter(3).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part1);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day03 Part1 duration: {:#?} \n", time_taken);
+    }
+    #[test]
+    fn test_day_03_pt2() {
+        use crate::day03::solve;
+        let iter = test_iter(3).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part2);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day03 Part2 duration: {:#?} \n", time_taken);
+    }
+
+    #[test]
+    fn test_day_04_pt1() {
+        use crate::day04::solve;
+        let iter = test_iter(4).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part1);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day04 Part1 duration: {:#?} \n", time_taken);
+    }
+    #[test]
+    fn test_day_04_pt2() {
+        use crate::day04::solve;
+        let iter = test_iter(4).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part2);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day04 Part2 duration: {:#?} \n", time_taken);
+    }
+
+    #[test]
+    fn test_day_05_pt1() {
+        use crate::day05::solve;
+        let iter = test_iter(5).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part1);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day05 Part1 duration: {:#?} \n", time_taken);
+    }
+    #[test]
+    fn test_day_05_pt2() {
+        use crate::day05::solve;
+        let iter = test_iter(5).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part2);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day05 Part2 duration: {:#?} \n", time_taken);
+    }
+
+    #[test]
+    fn test_day_06_pt1() {
+        use crate::day06::solve;
+        let iter = test_iter(6).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part1);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day06 Part1 duration: {:#?} \n", time_taken);
+    }
+    #[test]
+    fn test_day_06_pt2() {
+        use crate::day06::solve;
+        let iter = test_iter(6).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part2);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day06 Part2 duration: {:#?} \n", time_taken);
+    }
+
+    #[test]
+    fn test_day_07_pt1() {
+        use crate::day07::solve;
+        let iter = test_iter(7).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part1);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day07 Part1 duration: {:#?} \n", time_taken);
+    }
+    #[test]
+    fn test_day_07_pt2() {
+        use crate::day07::solve;
+        let iter = test_iter(7).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part2);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day07 Part2 duration: {:#?} \n", time_taken);
+    }
+
+    #[test]
+    fn test_day_08_pt1() {
+        use crate::day08::solve;
+        let iter = test_iter(8).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part1);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day08 Part1 duration: {:#?} \n", time_taken);
+    }
+    #[test]
+    fn test_day_08_pt2() {
+        use crate::day08::solve;
+        let iter = test_iter(8).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part2);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day08 Part2 duration: {:#?} \n", time_taken);
+    }
+
+    #[test]
+    fn test_day_09_pt1() {
+        use crate::day09::solve;
+        let iter = test_iter(9).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part1);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day09 Part1 duration: {:#?} \n", time_taken);
+    }
+    #[test]
+    fn test_day_09_pt2() {
+        use crate::day09::solve;
+        let iter = test_iter(9).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part2);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day09 Part2 duration: {:#?} \n", time_taken);
+    }
+
+    #[test]
+    fn test_day_10_pt1() {
+        use crate::day10::solve;
+        let iter = test_iter(10).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part1);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day10 Part1 duration: {:#?} \n", time_taken);
+    }
+    #[test]
+    fn test_day_10_pt2() {
+        use crate::day10::solve;
+        let iter = test_iter(10).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part2);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day10 Part2 duration: {:#?} \n", time_taken);
+    }
+
+    #[test]
+    fn test_day_11_pt1() {
+        use crate::day11::solve;
+        let iter = test_iter(11).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part1);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day11 Part1 duration: {:#?} \n", time_taken);
+    }
+    #[test]
+    fn test_day_11_pt2() {
+        use crate::day11::solve;
+        let iter = test_iter(11).unwrap();
+        let start = Instant::now();
+        
+        solve(iter, Part::Part2);
+        
+        let time_taken = Instant::now().duration_since(start);
+        print!("Day11 Part2 duration: {:#?} \n", time_taken);
+    }
+
+}
